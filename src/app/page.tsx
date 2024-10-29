58721ee6047a2581d8f3b91b2a24ca0379c61eb4
bmd816076 <100409081+BeladiyaMohit@users.noreply.github.com>
2024-10-29 19:23:50 +0530

    login page design done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+import IconFacebook from '@/components/icons/icon-facebook'
+import IconGoogle from '@/components/icons/icon-google'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { AtSign, TriangleAlert, User } from 'lucide-react'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid h-screen w-full gap-5 md:grid-cols-2 space-x-2">
+      <div className="relative hidden overflow-hidden bg-[#000000]  md:block md:h-full">
+        <div className="absolute left-1/2 top-1/4 w-full max-w-md -translate-x-1/2 space-y-3 px-3 text-center text-white">
+          <h2 className="text-lg font-bold sm:text-2xl lg:text-[30px]/9">
+            Social Media
+          </h2>
+          <p className="text-sm lg:text-xl/[30px]">
+            create your page for social page.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="flex overflow-y-auto">
+        <Card className="m-auto w-full max-w-[400px] space-y-[30px] p-5 shadow-2xl md:w-[400px]">
+          <CardHeader className="flex space-y-2 justify-center">
+            <h2 className="text-lg font-semibold text-black lg:text-xl/tight">
+              LOGO
+            </h2>
+          </CardHeader>
+          <CardContent className="space-y-[30px]">
+            <form className="space-y-[30px]">
+              <div className="relative space-y-3">
+                <label className="block font-semibold leading-none text-black">
+                  Email address
+                </label>
+                <Input
+                  type="email"
+                  variant={'input-form'}
+                  placeholder="username@domain.com"
+                  iconRight={
+                    <AtSign className="size-[18px]" />
+                  }
+                />
+              </div>
+              <div className="!mt-2.5 flex items-center gap-2">
+                <TriangleAlert className="size-[18px] shrink-0 text-danger" />
+                <p className="text-xs/tight font-medium text-danger">
+                  Please enter an email address in the format{' '}
+                  <span className="font-bold">
+                    username@gmail.com
+                  </span>
+                </p>
+              </div>
+              <div className="relative space-y-3">
+                <label className="block font-semibold leading-none text-black">
+                  Password
+                </label>
+                <Input
+                  type="password"
+                  variant={'input-form'}
+                  placeholder="Abc*********"
+                />
+              </div>
+              <Link
+                href="/forgot"
+                className="!mt-4 block text-right text-xs/4 font-semibold text-black underline underline-offset-[3px] hover:text-[#3C3C3D]"
+              >
+                Forgot password?
+              </Link>
+              <Button
+                type="submit"
+                variant={'black'}
+                size={'large'}
+                className="w-full"
+              >
+                Login
+              </Button>
+              <div className="text-center text-xs/4 font-semibold text-black">
+                Don’t have an account?
+                <Link
+                  href="/register"
+                  className="pl-1.5 text-sm/tight underline underline-offset-4 hover:text-[#3C3C3D]"
+                >
+                  Register
+                </Link>
+              </div>
+            </form>
+            <div className="grid grid-cols-2 gap-4">
+              <Button variant={'outline-general'} size={'large'}>
+                <IconGoogle className="!size-[18px]" />
+                Google
+              </Button>
+              <Button variant={'outline-general'} size={'large'}>
+                <IconFacebook className="!size-[18px] text-[#0866FF]" />
+                Facebook
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
